49930a27bf295cb9e4b996f6b9a26a14aedb5486
ikshan-Tango <ikshanbhardwaj.2003@gmail.com>
2023-04-09 02:23:20 +0530

    renewed

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres', 
         'USER': 'postgres', 
-        'PASSWORD': 'ikshan12340',
+        'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': 'localhost', 
         'PORT': '5432',
     }
