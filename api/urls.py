e06e9f5da50dfe20d2e2d0818f74e3d179cb8463
ikshan-Tango <ikshanbhardwaj.2003@gmail.com>
2023-04-08 21:10:11 +0530

    commit

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path,include
+from . import views
 from .views import HelloView
 
 
 urlpatterns = [
     path('',HelloView.as_view(),name="Hello"), #to get all paths
-]
+    path('ram/',view=views.RAMView.as_view(),name="Ram-details"), # to get ram details
+
+]   
