3bbb8c154c45950964cc887db9c572d95f06628c
ikshan-Tango <ikshanbhardwaj.2003@gmail.com>
2023-04-08 20:50:18 +0530

    added req.txt

--- api/models.py
+++ api/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+class RAM(models.Model):
+    used_memory = models.PositiveIntegerField(default=69)
+    total_memory = models.PositiveIntegerField(default=69)
+    used_swap = models.PositiveIntegerField(default=69)
+    total_swap =models.PositiveIntegerField(default=69) 
