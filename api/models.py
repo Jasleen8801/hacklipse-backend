c1a1361e6e209f66613f9c6853ac55f507972404
Akshat Jaimini <destrex271@gmail.com>
2023-04-09 18:40:20 +0530

    reverted

--- api/models.py
+++ api/models.py
@@ -25,7 +25,7 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
 
 #one user can have multiple devices
 class SysInfo(models.Model):
-    mac_id = models.PositiveIntegerField(primary_key = True,default=0) #it is actually the id of user
+    mac_id = models.PositiveIntegerField(default=0, unique=True) #it is actually the id of user
     kernel_version= models.CharField(max_length=25)
     host_name= models.CharField(max_length=50)
     name = models.CharField(max_length=20,default="ubuntu")
@@ -35,17 +35,17 @@ class SysInfo(models.Model):
     temp = models.JSONField(default=None, blank=True, null=True)
 
 class RAM(models.Model):
-    sys_id = models.ForeignKey(SysInfo, on_delete=models.CASCADE, default=0)
+    mac_id = models.ForeignKey(SysInfo, on_delete=models.CASCADE, default=0, to_field='mac_id')
     used_memory = models.PositiveIntegerField(default=69)
     total_memory = models.PositiveIntegerField(default=69)
     used_swap = models.PositiveIntegerField(default=69)
     total_swap =models.PositiveIntegerField(default=69)
 
     def _str_(self):
-        return str(self.sys_id)
+        return str(self.mac_id)
     
 class Process(models.Model):
-    sys_id = models.ForeignKey(SysInfo, on_delete=models.CASCADE,default=0)
+    mac_id = models.ForeignKey(SysInfo, on_delete=models.CASCADE,default=0,to_field='mac_id')
     pid = models.CharField(max_length=69)
     name = models.CharField(max_length=69)
     written_bytes = models.PositiveIntegerField(default=0)
@@ -54,4 +54,4 @@ class Process(models.Model):
     total_read_bytes = models.PositiveIntegerField(default=0)
 
     def _str_(self):
-        return str(self.p_id)
+        return str(self.pid)
